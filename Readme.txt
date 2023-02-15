d2cbf0dc1056cf9618a046bff10979e7c34ee29f
Rajesh <80874249+RajeshHsejar27@users.noreply.github.com>
2023-02-15 22:10:34 +0530

    Added required code files

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+1.Copy the Contents inside a folder.
+2.Make sure you have Node and npm installed and if not installed, install it first. Use npm install in the CMD in the project directory where the package file exists.
+This installs the required packages for the project.
+3.Create a OpenWeather account from Openweathermap.org and do the modifications to the code by using ur credentials in "app.js" file. The guidelines are commented for ur convenience and displayed there.
+3.Host it in Heroku, Netlify or Firebase (Much more if you are familiar with them).
+
+
+Note: While hosting on Heroku, you need to create a Procfile in the home project directory. Similar things have to followed when hosting on different sites. This project is a Nodejs Live Weather Display project.
+
+Screenshots have been added in the home directory after the successful testing in local host.
---
+++ app.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+
+});
+
+app.post("/",function(req,res){
+    
+    const query=req.body.cityname;
+ const url =
+    "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=somestuff";
+
+//insert ur app id from ur openweather account
+
+  https.get(url, function (response) {
+    
+
+    response.on("data", function (data) {
+      const wedata = JSON.parse(data);
+      const wedata1 = JSON.stringify(wedata);
+      const icon = wedata.weather[0].icon;
+      const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+const c=wedata.main.temp-273.15;
+
+      res.write("<p><h1 align=center>the weather condition in "+query+" is : " +
+          wedata.weather[0].description +
+          "</h1>"
+      );
+      res.write(
+        "<h1 align=center>the humidity condition in "+query+" is : " +
+          wedata.main.humidity +
+          "</h1>"
+      );
+ res.write(
+        "<h1 align=center>the temperature condition in "+query+" is : " +
+          c+
+          " degrees Celsius</h1>"
+      );
+ res.write(
+        "<h1 align=center>the pressure condition in "+query+" is : " +
+          wedata.main.pressure +
+          "</h1></p>"
+      );
+      res.write("<center><img width=500 height=500 src=" + imgUrl + "></center>");
+      res.send();
+    });
+  });
+
+
+})
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running on port 3000");
+})
