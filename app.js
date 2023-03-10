156f3cd580e370d66b4bbb40d5ef144b1cb8ab6c
Rajesh <80874249+RajeshHsejar27@users.noreply.github.com>
2023-03-10 21:29:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,18 +26,18 @@ app.post("/",function(req,res){
 
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
-      const wedata1 = JSON.stringify(wedata);
-      const icon = wedata.weather[0].icon;
+      const wedata1 = JSON.stringify(weatherData);
+      const icon = weatherData.weather[0].icon;
       const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 const c=wedata.main.temp-273.15;
 
       res.write("<p><h1 align=center>the weather condition in "+query+" is : " +
-          wedata.weather[0].description +
+          weatherData.weather[0].description +
           "</h1>"
       );
       res.write(
         "<h1 align=center>the humidity condition in "+query+" is : " +
-          wedata.main.humidity +
+          weatherData.main.humidity +
           "</h1>"
       );
  res.write(
@@ -47,7 +47,7 @@ const c=wedata.main.temp-273.15;
       );
  res.write(
         "<h1 align=center>the pressure condition in "+query+" is : " +
-          wedata.main.pressure +
+          weatherData.main.pressure +
           "</h1></p>"
       );
       res.write("<center><img width=500 height=500 src=" + imgUrl + "></center>");
